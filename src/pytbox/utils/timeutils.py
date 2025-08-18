cc6a6c2d91810df338aa845be60ae93f07c0fe0f
houm01 <houm01@foxmail.com>
2025-08-18 19:06:03 +0800

    Released v0.0.1

--- src/pytbox/utils/timeutils.py
+++ src/pytbox/utils/timeutils.py
@@ -34,3 +34,7 @@ class TimeUtils:
         '''
         if now:
             return datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+    
+    @staticmethod
+    def get_utc_time():
+        return datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
