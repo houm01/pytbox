271a22cece2c273252d6605b47506fca7544eef8
houm01 <houm01@foxmail.com>
2025-08-18 12:11:38 +0800

    Released v20250818.3: 删除了timeutils.py文件，并在base.py中移除了对TimeUtils的引用。同时在utils/timeutils.py中添加了获取当前时间对象的方法，支持时区信息。

--- src/pytbox/utils/timeutils.py
+++ src/pytbox/utils/timeutils.py
@@ -2,6 +2,8 @@
 
 
 import time
+import pytz
+import datetime
 
 
 class TimeUtils:
@@ -21,3 +23,14 @@ class TimeUtils:
             return int(time.time())
         else:
             return int(time.time() * 1000)
+    
+    @staticmethod
+    def get_time_object(now: bool=True):
+        '''
+        获取当前时间, 加入了时区信息, 简单是存储在 Mongo 中时格式为 ISODate
+
+        Returns:
+            current_time(class): 时间, 格式: 2024-04-23 16:48:11.591589+08:00
+        '''
+        if now:
+            return datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
