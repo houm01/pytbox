e48a90aa733fee1a9f7195ed20c0de26400a4d69
Your Name <you@example.com>
2025-08-22 02:14:34 +0800

    Released v0.0.8: 新增http_response支持，更新build_config.py以实现http_response模板渲染功能，并在instances.toml中添加http_response实例配置。同时更新pyproject.toml以包含相关模板文件。

--- src/pytbox/categraf/build_config.py
+++ src/pytbox/categraf/build_config.py
@@ -69,12 +69,23 @@ class BuildConfig:
         with open(Path(self.output_dir) / 'input.vsphere' / 'vsphere.toml', 'w', encoding='utf-8') as f:
             f.write(render_data)
 
+    def http_response(self):
+        template = self._get_template('input.http_response/http_response.toml.j2')
+        instances = self.instances['http_response']['instance']
+        render_data = template.render(instances=instances)
+        target_dir = Path(self.output_dir) / 'input.http_response'
+        if not target_dir.exists():
+            target_dir.mkdir(parents=True, exist_ok=True)
+        with open(Path(self.output_dir) / 'input.http_response' / 'http_response.toml', 'w', encoding='utf-8') as f:
+            f.write(render_data)
+            
     def run(self):
         # self.common('cpu')
         # self.common('mem')
         # self.common('net')
         # self.common('disk')
         # self.common('diskio')
-        self.vsphere()
+        # self.vsphere()
         # self.ping()
         # self.prometheus()   
+        self.http_response()
