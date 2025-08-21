0381e186abdca52f1501300520c432b4c26dd0f1
Your Name <you@example.com>
2025-08-22 02:04:29 +0800

    Released v0.0.7: 新增vsphere支持，添加多个输入模板文件（cpu、mem、net、disk、diskio），并在build_config.py中实现模板渲染功能。更新instances.toml以包含vsphere实例配置。

--- src/pytbox/categraf/build_config.py
+++ src/pytbox/categraf/build_config.py
@@ -26,8 +26,19 @@ class BuildConfig:
         self.instances = load_config_by_file(instances)
         self.output_dir = output_dir
     
-    
-    
+    def _get_template(self, template_name):
+        return env.get_template(template_name)
+
+    def common(self, input_name):
+        template = self._get_template(f'input.{input_name}/{input_name}.toml.j2')
+        render_data = template.render()
+        target_dir = Path(self.output_dir) / f'input.{input_name}'
+        if not target_dir.exists():
+            target_dir.mkdir(parents=True, exist_ok=True)
+            
+        with open(Path(self.output_dir) / f'input.{input_name}' / f'{input_name}.toml', 'w', encoding='utf-8') as f:
+            f.write(render_data)
+
     def ping(self):
         instances = self.instances['ping']['instance']
         render_data = ping_template.render(instances=instances)
@@ -44,10 +55,26 @@ class BuildConfig:
         target_dir = Path(self.output_dir) / 'input.prometheus'
         if not target_dir.exists():
             target_dir.mkdir(parents=True, exist_ok=True)
-            
         with open(Path(self.output_dir) / 'input.prometheus' / 'prometheus.toml', 'w', encoding='utf-8') as f:
             f.write(render_data)
 
+    def vsphere(self):
+        template = self._get_template('input.vsphere/vsphere.toml.j2')
+        instances = self.instances['vsphere']['instance']
+        print(instances)
+        render_data = template.render(instances=instances)
+        target_dir = Path(self.output_dir) / 'input.vsphere'
+        if not target_dir.exists():
+            target_dir.mkdir(parents=True, exist_ok=True)
+        with open(Path(self.output_dir) / 'input.vsphere' / 'vsphere.toml', 'w', encoding='utf-8') as f:
+            f.write(render_data)
+
     def run(self):
+        # self.common('cpu')
+        # self.common('mem')
+        # self.common('net')
+        # self.common('disk')
+        # self.common('diskio')
+        self.vsphere()
         # self.ping()
-        self.prometheus()        
+        # self.prometheus()   
