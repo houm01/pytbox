10d1bd9fb9452cec04de03bf0e6bf0ffa1771188
Your Name <you@example.com>
2026-01-03 23:50:03 +0800

    Released v0.4.8: update netbox lib

--- src/pytbox/nebtox/__init__.py
+++
@@ -1,3 +0,0 @@
-from .client import NetboxClient
-
-__all__ = ['NetboxClient']
--- src/pytbox/netbox/__init__.py
+++ src/pytbox/netbox/__init__.py
@@ -1,12 +1,3 @@
-"""
-Backwards/alternative import path for the NetBox client.
-
-Some users expect `pytbox.netbox`. The implementation lives in `pytbox.nebtox`,
-so we re-export here.
-"""
-
 from .client import NetboxClient
 
-__all__ = ["NetboxClient"]
-
-
+__all__ = ['NetboxClient']
